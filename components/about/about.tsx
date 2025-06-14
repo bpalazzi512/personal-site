e75136ad2458d88a2d702cb7b67c08851fedda04
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2025-06-14 14:53:16 -0400

    editing about page

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -1,5 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Github, LinkedinIcon } from 'lucide-react';
 import headshot from '../../public/headshot-no-bg.png';
 import Image from 'next/image'
 
@@ -14,17 +13,17 @@ export function About() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique animi excepturi natus earum! Eveniet ab magnam ad eum modi maiores dolorem nesciunt dolores quia quisquam similique voluptatem quam animi consectetur asperiores praesentium quaerat ipsum consequatur tenetur, expedita, exercitationem a atque nostrum. Consequatur distinctio ex ad maiores eaque pariatur, sequi totam!
             </h2>
             <div className="flex flex-row mt-6">
-                <a href="https://www.linkedin.com/in/bobby-palazzi/" className=" bg-[#0a66c2] px-3 py-1 rounded-md flex flex-row items-center justify-between mr-4 ">
-                    <FontAwesomeIcon icon={faLinkedin} size="1x" style={{color: "white"}}/>
-                    <h3 className="text-white text-lg ml-4">LinkedIn</h3>
+                <a href="https://www.linkedin.com/in/bobby-palazzi/">
+                    <LinkedinIcon color='#0a66c2' size="40px"/>
                 </a>
 
-                <a href="https://www.linkedin.com/in/bobby-palazzi/" className=" bg-[#2b3137] px-3 py-1 rounded-md flex flex-row items-center justify-between">
-                    <FontAwesomeIcon icon={faGithub} size="1x" style={{color: "white"}}/>
-                    <h3 className="text-white text-lg ml-4">Github</h3>
+                <a href="https://www.github.com/bpalazz512">
+                    <Github color='#2b3137' size="40px" />
                 </a>
+                
+                
             </div>
-            <a href="" className="bg-slate-100 px-4 rounded-md h-12 flex items-center justify-center mt-4">
+            <a href="" className="bg-slate-100 px-3 py-1 rounded-md h-12 flex items-center justify-center mt-4">
                 <h3 className="text-black text-lg">Resume Download</h3>
             </a>
         </div>
